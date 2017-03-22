0d4a9cd7956395556b2d0b715140e879437fb2db
twer <xiaotaozi1127@hotmail.com>
2017-03-22 11:19:56 +0800

    [taohui & shengzhi] #n/a set daterange as one year when choose month as period

--- javascripts/generateLineChart.js
+++ javascripts/generateLineChart.js
@@ -7,7 +7,6 @@
             minusDateNum = 140;
         } else if (period == 'month') {
             startDateObj.setDate(1);
-            startDateObj.setMonth((startDateObj.getMonth() + 1) % 12 + 1);
             minusDateNum = 365;
         } else if (period == 'year') {
             minusDateNum = 1000;
@@ -18,17 +17,15 @@
         startDateObj.setDate(startDateObj.getDate() - minusDateNum);
 
         var formatDate = function (startDateObj) {
-            var d = new Date(startDateObj), month = '' + (d.getMonth() + 1), day = '' + d.getDate(), year = d.getFullYear();
-            if (month.length < 2)
-                month = '0' + month;
-            if (day.length < 2)
-                day = '0' + day;
+            var d = new Date(startDateObj);
+            var month = d.getMonth() + 1;
+            var day = d.getDate();
+            var year = d.getFullYear();
             return [year, month, day].join('-');
         };
 
         var getDateLabel = function (e, period) {
             var label = e['label'];
-
             if (period == 'month') {
                 var date = new Date(label);
                 var month = ["January", "February", "March", "April", "May", "June",
